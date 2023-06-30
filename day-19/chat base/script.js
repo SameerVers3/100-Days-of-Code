44507b79577f4274f2361e5bfab38449e7dc1db4
SameerVers3 <sameerghafoor34@gmail.com>
2023-06-30 21:28:38 +0500

    day 19
    done with UI and now working on methods

--- day-19/chat base/script.js
+++ day-19/chat base/script.js
@@ -1,19 +1,68 @@
 const logBtn = document.getElementById("logBtn");
 const logInput = document.getElementById("log-input");
+const header = document.getElementById("heading");
+
+const bdy = document.getElementById("bdy");
+
+const renderLoginPage = () => {
+    console.log(header);
+    bdy.innerHTML = "";
+
+    let login = document.createElement("div");
+    login.classList.add("login");
+    let p = document.createElement("p");
+    p.textContent = "Enter the chat room with cool name: ";
+    
+    let logInput = document.createElement("div");
+    logInput.classList.add("login-input");
+
+    let txt = document.createElement("input");
+    txt.type = "text";
+    txt.placeholder =  "name";
+    txt.classList.add("log-input");
+    txt.maxLength = "15";
+    txt.id = "log-input";
+    
+    let log_btn = document.createElement("button");
+    log_btn.innerText = "Enter";
+    log_btn.classList.add("logBtn");
+    log_btn.id = "logBtn";
+
+    logInput.appendChild(txt);
+    logInput.appendChild(log_btn);
+
+    login.appendChild(p);
+    login.appendChild(logInput);
+
+    bdy.appendChild(login);
+
+}
 
 const checkSession = () => {
     if (localStorage.getItem("name") === null){
-        console.log("not loged in")
+        renderLoginPage();
     }
     else{
         console.log("loged in")
+        renderLoginPage();
     }
 }
 
-const setUser = () => {
-    let userName = logInput.value;
+const setUser = (value) => {
+    console.log("clicked")
+    let userName = value;
     localStorage.setItem("name", userName);
+    // console.log(header);
+    // header.style.height = "100px";
+    // console.log(header.style.height);
 }
 
 checkSession();
-logBtn.addEventListener("click", setUser)
+// logBtn.addEventListener("click", setUser)
+
+setTimeout(() => {
+    const logBtn = document.getElementById("logBtn");
+    const logInput = document.getElementById("log-input");
+
+    logBtn.addEventListener("click", setUser(logInput.value))
+}, 1000);
