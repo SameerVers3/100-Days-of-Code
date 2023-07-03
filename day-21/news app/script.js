15375230d912b709836de8e4905b41ffe9bbcb2c
SameerVers3 <sameerghafoor34@gmail.com>
2023-07-03 23:24:19 +0500

    day - 21
    news APP

---
+++ day-21/news app/script.js
@@ -0,0 +1,10 @@
+let api = ``
+let baseURL = `https://newsapi.org/v2/everything?q=`
+
+window.addEventListener("load", () => {
+    fetchNews("pakistan");
+});
+
+async function fetch(query) {
+    const async = await 
+}
