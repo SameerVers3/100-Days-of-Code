b8053b568306d3cf61f70c822f3ab6f7ae803b39
SameerVers3 <sameerghafoor34@gmail.com>
2023-07-03 23:38:36 +0500

    bugs bugs bugs

--- day-21/news app/script.js
+++ day-21/news app/script.js
@@ -1,10 +1,30 @@
-let api = ``
+let api = `62961a8919ea409e84029eefff95f6a9`
 let baseURL = `https://newsapi.org/v2/everything?q=`
 
 window.addEventListener("load", () => {
     fetchNews("pakistan");
 });
 
-async function fetch(query) {
-    const async = await 
+async function fetchNews(query) {
+    const res = await fetch(`${baseURL}${query}&apiKey=${api}`)
+    const data = await res.json();
+    bindData(data.articles);
 }
+
+function bindData(articles) {
+    const cardsContainer =  document.getElementById("cards-contanier");
+    const newsCardTemplate = document.getElementById("template-news-card");
+
+    cardsContainer.innerHTML = "";
+
+    articles.forEach((articles) => {
+    if (!articles.urlToImage) return
+        const cardClone = newsCardTemplate.content.cloneNode(true);
+        fillDataInCard(cardClone, articles);
+        cardsContainer.appendChild(cardClone);
+    });
+}
+
+// function fillDataInCard(cardClone, article){
+//     const newImg = cardClone.querySelle
+// }
