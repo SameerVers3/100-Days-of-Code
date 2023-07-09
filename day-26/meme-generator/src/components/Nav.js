d847c4bbed0ec94724e9b8fbd1bf90d80b7ed9ff
SameerVers3 <sameerghafoor34@gmail.com>
2023-07-09 23:52:40 +0500

    Meme generator: minor changes

--- day-26/meme-generator/src/components/Nav.js
+++ day-26/meme-generator/src/components/Nav.js
@@ -1,15 +1,17 @@
 import React from "react"
+import icon from "../images/icon.png"
+
 
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="../images/icon.png" 
+                src={icon}
                 className="header--image"
                 alt="icon"
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h4 className="header--project">Have Fun</h4>
         </header>
     )
 }
